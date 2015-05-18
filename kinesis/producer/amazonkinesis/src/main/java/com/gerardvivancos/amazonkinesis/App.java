ad22365c296e957a90167e397849d4dcd620624e
Gerard Vivancos <gerard.vivancos@gmail.com>
2015-05-18 16:51:08 +0200

    Maven init of the Java app

---
+++ kinesis/producer/amazonkinesis/src/main/java/com/gerardvivancos/amazonkinesis/App.java
@@ -0,0 +1,13 @@
+package com.gerardvivancos.amazonkinesis;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
