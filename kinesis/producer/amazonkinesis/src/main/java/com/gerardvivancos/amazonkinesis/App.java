2a318ab9b64426141f10f8a3c895e02bdfaf6afd
Gerard Vivancos <gerard.vivancos@gmail.com>
2015-05-18 17:43:17 +0200

    Hello world is now describe regions

--- kinesis/producer/amazonkinesis/src/main/java/com/gerardvivancos/amazonkinesis/App.java
+++ kinesis/producer/amazonkinesis/src/main/java/com/gerardvivancos/amazonkinesis/App.java
@@ -1,5 +1,5 @@
 package com.gerardvivancos.amazonkinesis;
-
+import com.amazonaws.regions.RegionUtils;
 /**
  * Hello world!
  *
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( RegionUtils.getRegions() );
     }
 }
